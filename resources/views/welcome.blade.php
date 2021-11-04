623466ff451700d594b5b2d9c253f5568ab21790
unknown <krishnalodh3@gmail.com>
2021-11-04 12:10:19 +0530

    changed to larabiz2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Larabiz
+                    Larabiz testing
                 </div>
 
                 <div class="links">
